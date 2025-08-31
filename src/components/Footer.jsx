f0f5d31244660ab3694ea42fbe9cc3e723932990
jpkam <jpkamdem@proton.me>
2025-08-31 11:46:23 +0200

    deploiement

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="h-26 bg-violet text-custom-rose drop-shadow-[0px_5px_20px_black] flex justify-center items-center">
       <span className="before:mr-1 before:content-['©']">
-        Festival MMI 2023. Tous droits réservés.
+        Festival MMI 2025. Tous droits réservés.
       </span>
     </footer>
   );
