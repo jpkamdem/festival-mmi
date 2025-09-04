1b562586bafd1d7b685bd43b954dcdc55654a4ef
jpkam <jpkamdem@proton.me>
2025-09-04 20:16:37 +0200

    path fix

--- src/main.jsx
+++ src/main.jsx
@@ -4,36 +4,39 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 import About from './pages/About.jsx';
-import Connexion from './pages/Connexion.jsx';
 import Home from './pages/Home.jsx';
-import Laureats from './pages/Laureats.jsx';
 import NotFound from './pages/NotFound.jsx';
 import Selection from './pages/Selection.jsx';
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
+	[
+		{
+			path: '/',
+			element: <App />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+				},
+				{
+					path: '/selection',
+					element: <Selection />,
+				},
+				{
+					path: '/a-propos',
+					element: <About />,
+				},
+				{
+					path: '*',
+					element: <NotFound />,
+				},
+			],
+		},
+	],
 	{
-		path: '/',
-		element: <App />,
-		children: [
-			{
-				path: '/',
-				element: <Home />,
-			},
-			{
-				path: '/selection',
-				element: <Selection />,
-			},
-			{
-				path: '/a-propos',
-				element: <About />,
-			},
-			{
-				path: '*',
-				element: <NotFound />,
-			},
-		],
-	},
-]);
+		basename: import.meta.env.BASE_URL,
+	}
+);
 
 createRoot(document.getElementById('root')).render(
 	<StrictMode>
